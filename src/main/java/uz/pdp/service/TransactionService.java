37b23a5805c0277e2594b0065c135b8aa7ab8e77
MadiyorNegov <madiyor033@gmail.com>
2024-05-08 16:25:09 +0500

    ddss

--- src/main/java/uz/pdp/service/TransactionService.java
+++ src/main/java/uz/pdp/service/TransactionService.java
@@ -21,28 +21,35 @@ public class TransactionService extends BaseService<Transaction, TransactionRepo
     public boolean check(Transaction transaction) {
         return false;
     }
+
     public List<Transaction> getUserTransactions(UUID userId) {
         return repository.getAllUserTransactions(userId);
     }
+
     public void addTransaction(UUID fromCard, UUID toCard, double amount) throws DataNotFoundException {
         repository.addTransaction(fromCard, toCard, amount);
     }
+
     public List<Transaction> getAllTransactionsFromFile() {
         return repository.getAllTransactionsFromFile();
     }
+
     public List<Transaction> getUserTransactionsInPeriod(UUID userId, LocalDateTime date1, LocalDateTime date2) {
-        return repository.getUserTransactionsInPeriod(userId,date1,date2);
+        return repository.getUserTransactionsInPeriod(userId, date1, date2);
     }
-    public List<Transaction> getOutcomeTransactions(UUID id){
+
+    public List<Transaction> getOutcomeTransactions(UUID id) {
         return repository.getOutcomeTransactions(id);
     }
-    public List<Transaction> getIncomeTransactions(UUID id){
+
+    public List<Transaction> getIncomeTransactions(UUID id) {
         return repository.getIncomeTransactions(id);
     }
 
     public List<Transaction> getAllUserIncomeTransactions(UUID userId) {
         return repository.getAllUserIncomeTransactions(userId);
     }
+
     public List<Transaction> getAllUserOutComeTransactions(UUID userId) {
         return repository.getAllUserOutComeTransactions(userId);
     }
